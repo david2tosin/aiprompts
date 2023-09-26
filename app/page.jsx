79514c4f841bd2e987cb56e0c2bda819eb821364
david2tosin <david2tosin@gmail.com>
2023-09-26 15:10:15 +0100

    fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import Feed from "@components/Feed"
+import Feed from "@components/Feed";
 
 // const fetchPosts = async () => {
 //   const response = await fetch("http:/localhost:3000/api/prompt");
@@ -8,23 +8,23 @@ import Feed from "@components/Feed"
 // };
 
 const Home = async () => {
-
   // const data = await fetchPosts();
   // console.log(data);
 
   return (
-    <section className="w-full flex-center flex-col">
+    <section className="flex-center w-full flex-col">
       <h1 className="head_text text-center">
         Discover & Share
-        <br className="max-md:hidden"/>
+        <br className="max-md:hidden" />
         <span className="orange_gradient text-center"> AI-powered Prompts</span>
       </h1>
       <p className="desc text-center">
-         Promptopia is an open-source AI prompting tool for modern world to discover, create and share prompts.
+        Promptzz is an open-source AI prompting tool for the modern world to
+        discover, create and share prompts.
       </p>
 
       <Feed />
     </section>
-  )
-}
-export default Home
+  );
+};
+export default Home;
